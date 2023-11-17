8a3c63ad2a194ea03b6f5172edfc959c71e3d08c
ednam34 <rayanoo1302@gmail.com>
2023-11-17 09:42:29 +0100

    host check

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      'http://rayanekaabeche.fr:8080/',
+      'http://rayanekaabeche.fr',
+    ],
+  },
   transpileDependencies: true
 })
